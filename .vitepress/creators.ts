64aaee36e86694a863655961d6f176393b39561b
PFF <duronfly@163.com>
2025-06-09 10:45:45 +0800

    1

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -25,12 +25,6 @@ export const creators: Creator[] = [
     username: 'dandan',
     title: '百搭 原始创作者',
     desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
-    links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
-    ],
-    nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
-    emailAliases: ['neko@ayaka.moe'],
   },
   {
     name: '忧伤',
@@ -38,12 +32,6 @@ export const creators: Creator[] = [
     username: 'youshang',
     title: '百搭 原始创作者',
     desc: '开源开发者，专注于前端，以及前端相关工具库和工具链开发',
-    links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/LittleSound' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/OikawaRizumu' },
-    ],
-    nameAliases: ['LittleSound', '绚香音', '絢香音', 'Rizumu Oikawa', 'Rizumu Ayaka', 'Ayaka Rizumu', 'Rizumu'],
-    emailAliases: ['rizumu@ayaka.moe', 'rizumu@oqo.moe'],
   },
 ].map<Creator>((c) => {
   c.avatar = c.avatar || getAvatarUrl(c.username)
