66e7a3e7a34041e1dbb3845e2ab379272c68e1ca
蛋蛋 <duronfly@163.com>
2025-06-26 17:37:22 +0800

    去记录

--- vite.config.ts
+++ vite.config.ts
@@ -24,18 +24,15 @@ export default defineConfig(async () => {
       GitChangelog({
         repoURL: () => githubRepoLink,
       }),
-      GitChangelogMarkdownSection({
-        getChangelogTitle: (): string => {
-          return '文件历史'
-        },
-        getContributorsTitle: (): string => {
-          return '贡献者'
-        },
-        excludes: [
-          'toc.md',
-          'index.md',
-        ],
-      }),
+      // GitChangelogMarkdownSection({
+      //   getContributorsTitle: (): string => {
+      //     return '贡献者'
+      //   },
+      //   excludes: [
+      //     'toc.md',
+      //     'index.md',
+      //   ],
+      // }),
       PageProperties(),
       PagePropertiesMarkdownSection({
         excludes: [
