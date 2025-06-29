a3f66033769eaf4ab02156aeb82906ef0450d562
蛋蛋 <duronfly@gmail.com>
2025-06-29 16:24:06 +0800

    11

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -145,6 +145,8 @@ export default defineConfig({
       var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);
       pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};
     `],
+    // Mondiad
+    ['script', { 'async': 'true', 'src': 'https://ss.mrmnd.com/banner.js' }],
   ],
   themeConfig: {
     outline: { label: '页面大纲', level: 'deep' },
